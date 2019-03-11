758252934963e431fa9f1d37ae2d3e94fe1ce606
jwhong98 <jwhong98@gmail.com>
2019-03-10 21:30:05 -0700

    Update hwk4.cpp

--- hwk4.cpp
+++ hwk4.cpp
@@ -2,6 +2,10 @@
 #include <string>
 using namespace std;
 
+typedef int boolean;
+#define true 1
+#define false 0
+
 class ParkedCar {
 private:
 	string make;
@@ -9,133 +13,161 @@ private:
 	string color;
 	string licenseNumber;
 	int minutesParked;
-
 public:
-	ParkedCar(string mk, string mod, string col, string lic, int min) {
-		make = mk;
-		model = mod;
-		color = col;
-		licenseNumber = lic;
-		minutesParked = min;
-	}
+	ParkedCar(string m, string mod, string col, string ln, int min);
 
-	ParkedCar(ParkedCar car2) {
-		make = car2.make;
-		model = car2.model;
-		color = car2.color;
-		licenseNumber = car2.licenseNumber;
-		minutesParked = car2.minutesParked;
-	}
+	ParkedCar(ParkedCar *car2);
 
-	void setMinutesParked(int min) {
-		minutesParked = min;
+	string getMake() {
+		return make;
 	}
-
+	string getModel() {
+		return model;
+	}
+	string getColor() {
+		return color;
+	}
+	string getLicenseNumber() {
+		return licenseNumber;
+	}
 	int getMinutesParked() {
 		return minutesParked;
 	}
-
-	/*string toString() {
-		
-	}*/
+	string setMake(string m) {
+		make = m;
+	}
+	string setModel(string mod) {
+		model = mod;
+	}
+	string setColor(string col) {
+		color = col;
+	}
+	string setLicenseNumber(string ln) {
+		licenseNumber = ln;
+	}
+	int setMinutesParked(int min) {
+		minutesParked = min;
+	}
 };
+
+ParkedCar::ParkedCar(string m, string mod, string col, string ln, int min) {
+	make = m;
+	model = mod;
+	color = col;
+	licenseNumber = ln;
+	minutesParked = min;
+}
+
+ParkedCar::ParkedCar(ParkedCar *car2) {
+	this->make = car2->make;
+	this->model = car2->model;
+	this->color = car2->color;
+	this->licenseNumber = car2->licenseNumber;
+	this->minutesParked = car2->minutesParked;
+}
 
 class ParkingMeter {
 private:
 	int minutesPurchased;
-
 public:
 	ParkingMeter(int m) {
 		minutesPurchased = m;
 	}
-
+	int getMinutesPurchased() {
+		return minutesPurchased;
+	}
 	void setMinutesPurchased(int m) {
 		minutesPurchased = m;
 	}
+};
 
-	int getMinutesPurchased() {
-		return minutesPurchased;
+class PoliceOfficer {
+private:
+	string name;
+	int badgeNumber;
+
+public:
+	PoliceOfficer(string n, int bn) {
+		name = n;
+		badgeNumber = bn;
+	}
+
+	string getName() {
+		return name;
+	}
+	int getBadgeNumber() {
+		return badgeNumber;
+	}
+	void setName(string n) {
+		name = n;
+	}
+	void setBadgeNumber(int bn) {
+		badgeNumber = bn;
+	}
+
+	boolean patrol(int timePurchased, int timeParked) {
+		if (timeParked - timePurchased > 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 };
 
 class ParkingTicket {
+
 private:
-	ParkedCar car;
-	PoliceOfficer officer;
-	double fine;
-	int minutes;
-	double baseFine = 25.00;
-	double hourlyFine = 10.00;
-
+	int fine;
+	int overtime;
 public:
-	ParkingTicket(ParkedCar aCar, PoliceOfficer anOfficer, int min) {
-		car = aCar;
-		officer = anOfficer;
-		minutes = min;
-		calculateFine();
-	}
-
-	ParkingTicket(ParkingTicket ticket2) {
-		car = ticket2.car;
-		officer = ticket2.officer;
-		minutes = ticket2.minutes;
-	}
-
-	void calculateFine() {
-		double hours = minutes / 60.0;
-		int hoursAsInt = (int)hours;
-
-		if ((hours - hoursAsInt) > 0) {
-			hoursAsInt++;
-		}
-		fine = baseFine;
-		fine += (hoursAsInt + hourlyFine);
-	}
-
-	void setFine(double f) {
-		fine = f;
-	}
-
-	double getFine() {
+	int getFine() {
 		return fine;
 	}
-
-	ParkedCar getCar() {
-		return car;
+	void setFine(int fine) {
+		this->fine = fine;
 	}
+	void ticket(ParkedCar aCar, ParkingMeter meter, PoliceOfficer anOfficer) {
+		cout << "Car's make: " << aCar.getMake() << endl;
+		cout << "Car's model: " << aCar.getModel() << endl;
+		cout << "Car's color: " << aCar.getColor() << endl;
+		cout << "Car's license number: " << aCar.getLicenseNumber() << endl;
 
-	PoliceOfficer getOfficer() {
-		return officer;
-	}
-
-	int getMinutes() {
-		return minutes;
+		overtime = ((aCar.getMinutesParked() - meter.getMinutesPurchased()) / 60) + 1;
+		if (overtime <= 1) {
+			setFine(25);
+		}
+		else {
+			setFine(25 + ((overtime - 1) * 10));
+			cout << "Fine: $" << getFine() << endl;
+			cout << "Officer: " << anOfficer.getName() << endl;
+			cout << "Badge number: " << anOfficer.getBadgeNumber() << endl;
+		}
 	}
 };
 
-	class PoliceOfficer {
-	private:
-		string name;
-		string badgeNumber;
 
-	public:
-		PoliceOfficer(string n, string bn) {
-			name = n;
-			badgeNumber = bn;
-		}
-
-		ParkingTicket patrol(ParkedCar car, ParkingMeter meter) {
-			ParkingTicket ticket = null;
-			int illegalMinutes = car.getMinutesParked() - meter.getMinutesPurchased();
-
-			if (illegalMinutes > 0) {
-				ticket = new ParkingTicket(car, this, illegalMinutes);
-			}
-			return ticket;
-		}
-};
 
 int main() {
 
-	ParkedCar = new ParkedCar
+	boolean status;
+
+	ParkedCar car("Lexus", "2008", "White", "147RHZM", 125);
+
+	ParkingMeter meter(60);
+
+	PoliceOfficer officer("Paul Blart", 4788);
+
+	status = officer.patrol(meter.getMinutesPurchased(), car.getMinutesParked());
+	if (status == true) {
+		ParkingTicket *ticket = new ParkingTicket();
+		ticket->ticket(car, meter, officer);
+	}
+	else {
+		cout << "Car is legallt parked." << endl;
+	}
+
+	std::cin.get();
+
+	return 0;
 }
