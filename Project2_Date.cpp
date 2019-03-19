37ec661595090eb310ac7538c232bb56e43c51ec
jwhong98 <jwhong98@gmail.com>
2019-03-19 09:55:06 -0700

    Update Project2_Date.cpp

--- Project2_Date.cpp
+++ Project2_Date.cpp
@@ -1,95 +1,232 @@
 #include <iostream>
-#include <string>
+#include<string>
 using namespace std;
 
-class Date {
+class Date{
 private:
-	string month;
-	int mont;
+	int month;
+	int day;
+	int year;
+	int days[12];
+public:
+	Date(int m, int d, int y){
+		month = m;
+		day = d;
+		year = y;
+	}
+	void setDaysInMonth(){
+		days[0] = 31;
+		days[1] = 28;
+		days[2] = 31;
+		days[3] = 30;
+		days[4] = 31;
+		days[5] = 30;
+		days[6] = 31;
+		days[7] = 30;
+		days[8] = 30;
+		days[9] = 31;
+		days[10] = 30;
+		days[11] = 31;
+	}
+	void printDate(){
+		string mon[] = {"January", "February", "March", "April", "May", "June",
+				"July", "August", "September", "October", "November", "December"};
+		cout << mon[month - 1] << " " << day << ", " << year << endl;
+	}
+	int getMonth(){
+		return month;
+	}
+	int getDay(){
+		return day;
+	}
+	int getYear(){
+		return year;
+	}
+	void setMonth(int m){
+		month = m;
+	}
+	void setDay(int d){
+		day = d;
+	}
+	void setYear(int y){
+		year = y;
+	}
+	Date &operator ++(){
+		Date date(month, day, year);
+		if(day == days[month - 1]){
+			day = 1;
+			month++;
+			month %= 12;
+			if(month == 1){
+				year++;
+			}
+		}else{
+			day++;
+		}
+		return date;
+	}
+	Date operator ++(int){
+		Date date(month, day, year);
+		if(day == days[month - 1]){
+			day = 1;
+			month++;
+			month %= 12;
+			if(month == 1){
+				year++;
+			}
+		}else{
+			day++;
+		}
+		return date;
+	}
+	Date &operator --(){
+		Date date(month, day, year);
+		day--;
+		if(day == 0){
+			month--;
+			if(month == 0){
+				month = 12;
+				year--;
+			}
+			day = days[month - 1];
+		}
+		return date;
+	}
+	Date operator --(int){
+		Date date(month, day, year);
+		day--;
+		if(day == 0){
+			month --;
+			if(month == 0){
+				month = 12;
+				year --;
+			}
+			day = days[month - 1];
+		}
+		return date;
+	}
+};
+
+int realDate(int m, int d){
+	if(d <= 0){
+		return 0;
+	}
+	switch(m){
+	case 1: if(d > 31){
+			return 0;
+			}else{
+				return 1;
+			}
+		break;
+	case 2: if(d > 28){
+				return 0;
+			}else{
+				return 1;
+			}
+		break;
+	case 3: if(d > 31){
+				return 0;
+			}else{
+				return 1;
+			}
+		break;
+	case 4: if(d > 30){
+				return 0;
+			}else{
+				return 1;
+			}
+		break;
+	case 5: if(d > 31){
+			return 0;
+			}else{
+				return 1;
+			}
+		break;
+	case 6: if(d > 30){
+			return 0;
+			}else{
+				return 1;
+			}
+		break;
+	case 7 : if(d > 31){
+				return 0;
+			}else{
+				return 1;
+			}
+		break;
+	case 8: if(d > 31){
+				return 0;
+			}else{
+				return 1;
+			}
+		break;
+	case 9: if(d > 30){
+				return 0;
+			}else{
+				return 1;
+			}
+		break;
+	case 10: if(d > 31){
+				return 0;
+			}else{
+				return 1;
+			}
+		break;
+	case 11: if(d > 30){
+				return 0;
+			}else{
+				return 1;
+			}
+		break;
+		case 12: if(d > 2831){
+					return 0;
+				}else{
+					return 1;
+				}
+		break;
+		default: return 0;
+		break;
+	}
+}
+int main(){
+	int month;
 	int day;
 	int year;
 
-public:
-	Date(int m, int d, int y) {
-		mont = m;
-		day = d;
-		year = y;
-	}
-	Date(string m, int d, int y) {
-		month = m;
-		day = d;
-		year = y;
-	}
-	Date(int d, string m, int y) {
-		day = d;
-		month = m;
-		year = y;
-	}
-	void printNumerical() {
-		if (mont > 12 or day > 31) {
-			cout << "Invalid date." << endl;
-		}
-		else {
-			cout << mont << "/" << day << "/" << year << endl;
-		}
-	}
-	void printMonthFirst() {
-		if (day > 31) {
-			cout << "Invalid date." << endl;
-		}
-		else {
-			cout << month << day << ", " << year << endl;
-		}
-	}
-	void printDayFirst() {
-		if (day > 31) {
-			cout << "Invalid date." << endl;
-		}
-		else {
-			cout << day << month << year << endl;
-		}
-	}
-	void incrementDayPre(Date date) {
-		++day;
-	}
-	void incrementDayPost(Date date) {
-		day++;
-	}
-	void decrementDayPre(Date date) {
-		--day;
-	}
-	void decrementDayPost(Date date) {
-		day--;
-	}
-	int subtractDate(Date date, Date date1) {
-		if (date.getDay() < date1.getDay()) {
-			cout << "Input larger date first" << endl;
-		}
-		else {
-			day = date.getDay() - date1.getDay();
-			return day;
-		}
-	}
-	int getDay() {
-		return day;
-	}
-	void displayDate(Date date) {
-		cout << month << " " << day << ", " << year << endl;
-	}
+	cout << "Enter month: ";
+	cin >> month;
+	cout << "\nEnter day: ";
+	cin >> day;
+	cout << "\nEnter year: ";
+	cin >> year;
+	cout << endl;
 
-};
+	int real = realDate(month, day);
 
-int main() {
-
-	Date date1(12, 25, 2014);
-	Date date2("December", 25, 2014);
-	Date date3(25, "December", 2014);
-
-	date1.printNumerical();
-	date2.printMonthFirst();
-	date3.printDayFirst();
-
-
-	std::cin.get();
-		return 0;
+	if(real == 0){
+		cout << "Invalid date";
+	}else{
+		Date date(month, day, year);
+		date.setDaysInMonth();
+		cout << "Current Date: ";
+		date.printDate();
+		cout << endl;
+		cout << "Prefix incremented date: ";
+		++date;
+		date.printDate();
+		cout << endl;
+		cout << "Postfix incremented date: ";
+		date++;
+		date.printDate();
+		cout << endl;
+		cout << "Prefix decremented date: ";
+		--date;
+		date.printDate();
+		cout << endl;
+		cout << "Postfix decremented date: ";
+		date--;
+		date.printDate();
+	}
 }
+
+
