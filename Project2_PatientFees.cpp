6d582cd177289e87c748d5814e24f7d654a8b589
jwhong98 <jwhong98@gmail.com>
2019-03-18 23:54:44 -0700

    Update Project2_PatientFees.cpp

--- Project2_PatientFees.cpp
+++ Project2_PatientFees.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
 using namespace std;
 
-class PatientAccount{
+typedef int boolean;
+#define true 1
+#define false 0
+
+class PatientAccount {
 private:
 	int daysInHospital;
-	const int dailyRate = 0;
+	const int dailyRate = 2000;
 public:
-	int charges = 0;
-	PatientAccount(int days){
+	static int charges;
+	PatientAccount(int days) {
 		daysInHospital = days;
 	}
-	int charge(PatientAccount account){
+	int charge(PatientAccount account) {
 		charges = daysInHospital * dailyRate;
+		return charges;
+	}
+	int getCharges() {
 		return charges;
 	}
 };
 
-class Surgery{
+int PatientAccount::charges = 0;
+
+class Surgery {
 private:
 	const int surgery1Price = 100;
 	const int surgery2Price = 200;
@@ -25,24 +34,25 @@ private:
 	const int surgery5Price = 500;
 
 public:
-	int getSurgery1Price(){
-		return surgery1Price;
+
+	int addSurgery1Price() {
+		return PatientAccount::charges += surgery1Price;
 	}
-	int getSurgery2Price(){
-			return surgery2Price;
-		}
-	int getSurgery3Price(){
-			return surgery3Price;
-		}
-	int getSurgery4Price(){
-			return surgery4Price;
-		}
-	int getSurgery5Price(){
-			return surgery5Price;
-		}
+	int addSurgery2Price() {
+		return PatientAccount::charges += surgery2Price;
+	}
+	int addSurgery3Price() {
+		return PatientAccount::charges += surgery3Price;
+	}
+	int addSurgery4Price() {
+		return PatientAccount::charges += surgery4Price;
+	}
+	int addSurgery5Price() {
+		return PatientAccount::charges += surgery5Price;
+	}
 };
 
-class Pharmacy{
+class Pharmacy {
 private:
 	const int med1 = 10;
 	const int med2 = 20;
@@ -51,24 +61,118 @@ private:
 	const int med5 = 50;
 
 public:
-	int getmedication1Price(){
-		return med1;
+
+	int addMedication1Price() {
+		return PatientAccount::charges += med1;
 	}
-	int getmedication2Price(){
-			return med2;
-		}
-	int getmedication3Price(){
-			return med3;
-		}
-	int getmedication4Price(){
-			return med4;
-		}
-	int getmedication5Price(){
-			return med5;
-		}
+	int addMedication2Price() {
+		return PatientAccount::charges += med2;
+	}
+	int addMedication3Price() {
+		return PatientAccount::charges += med3;
+	}
+	int addMedication4Price() {
+		return PatientAccount::charges += med4;
+	}
+	int addMedication5Price() {
+		return PatientAccount::charges += med5;
+	}
 };
 
-int main(){
+void mainMenu();
+void surgeryMenu();
+void medicationMenu();
 
-	return 0;
+int main() {
+
+	PatientAccount patient(5); //assume patient stayed for 5 days
+	patient.charge(patient);
+
+	mainMenu();
+
+	cout << patient.getCharges();
+
+	//std::cin.get();
+
+
+}
+
+void mainMenu() {
+	int selection;
+	boolean run = true;
+		cout << "Choose option number: \n1. Surgery type\n2. Medication type\n3. Exit" << endl;
+
+		cin >> selection;
+
+		switch (selection) {
+		case 1: surgeryMenu();
+			break;
+		case 2: medicationMenu();
+			break;
+		case 3: cout << "Current charges: $";
+			break;
+		default: cout << "Select 1, 2, or 3" << endl;
+			break;
+		}
+
+}
+void surgeryMenu() {
+
+	Surgery surgery;
+
+	int selection;
+	boolean run = true;
+	while (run) {
+		cout << "Select surgery to add: \n1. Surgery 1\n2. Surgery 2\n3. Surgery 3\n4. Surgery 4\n5. Surgery 5\n6. Previous menu" << endl;
+
+		cin >> selection;
+
+		switch (selection) {
+		case 1: surgery.addSurgery1Price();
+			break;
+		case 2: surgery.addSurgery2Price();
+			break;
+		case 3: surgery.addSurgery3Price();
+			break;
+		case 4: surgery.addSurgery4Price();
+			break;
+		case 5: surgery.addSurgery5Price();
+			break;
+		case 6: run = false;
+				mainMenu();
+			break;
+		default: cout << "Select a valid surgery or return to previous menu." << endl;
+			break;
+		}
+	}
+}
+
+void medicationMenu() {
+
+	Pharmacy pharmacy;
+
+	int selection;
+	boolean run = true;
+	while (run) {
+		cout << "Select medication to add: \n1. Medication 1\n2. Medication 2\n3. Medication 3\n4. Meciation 4\n5. Medication 5\n6. Previous menu" << endl;
+
+		cin >> selection;
+
+		switch (selection) {
+		case 1: pharmacy.addMedication1Price();
+			break;
+		case 2: pharmacy.addMedication2Price();
+			break;
+		case 3: pharmacy.addMedication3Price();
+			break;
+		case 4: pharmacy.addMedication4Price();
+			break;
+		case 5: pharmacy.addMedication5Price();
+			break;
+		case 6: run = false;
+				mainMenu();
+			break;
+		default: cout << "Select valid medication or return to previous menu." << endl;
+		}
+	}
 }
